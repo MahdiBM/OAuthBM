98ddbce6926d2fdc8198d33a230ee5776adee362
MahdiBM <54685446+MahdiBM@users.noreply.github.com>
2021-06-04 17:03:37 +0430

    Moved 2 extensions in tests to their appropriate file to remove possible confusion

--- Tests/OAuthBMTests/Preperation/OAuthTokens Table.swift
+++ Tests/OAuthBMTests/Preperation/OAuthTokens Table.swift
@@ -97,3 +97,21 @@ extension OAuthTokens {
         }
     }
 }
+
+private extension FieldProperty {
+    /// Initializes an instance of FieldProperty.
+    /// - Parameter key: A value conforming to RawRepresentable where RawValue is String.
+    convenience init<Value>(key: Value)
+    where Value: RawRepresentable, Value.RawValue == String {
+        self.init(key: .init(stringLiteral: key.rawValue))
+    }
+}
+
+private extension FieldKey {
+    /// Initializes an instance of FieldKey.
+    /// - Parameter key: A value conforming to RawRepresentable where RawValue is String.
+    init<Value>(_ key: Value)
+    where Value: RawRepresentable, Value.RawValue == String {
+        self.init(stringLiteral: key.rawValue)
+    }
+}
